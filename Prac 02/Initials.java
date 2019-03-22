179991b5c780703f487ab7c7caeeb92506ed53d6
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-22 12:16:42 +0800

    complete Initials exercise

---
+++ Prac 02/Initials.java
@@ -0,0 +1,11 @@
+public class Initials {
+    public static void main(String[] args){
+        final char PERIOD = '.';
+        char initial1 = 'P';
+        char initial2 = 'J';
+        char initial3 = 'W';
+
+        System.out.println("Name Initial: " + initial1 + PERIOD + initial2 + PERIOD + initial3);
+
+    }
+}
