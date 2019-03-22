938e5236fdd0ee9cfd92f8d2a7949c394e5f5a18
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-22 11:31:30 +0800

    complete NauticalMilesInteractive exercise

--- Prac 02/NauticalMilesInteractive.java
+++ Prac 02/NauticalMilesInteractive.java
@@ -1,7 +1,19 @@
+import javax.swing.*;
+
 public class NauticalMilesInteractive {
     public static void main(String[] args){
-        double kmInNauticalMile = 1.852;
-        double milesInNauticalMile = 1.150779;
-        double nauticalMiles = 10.0;
+        final double KMINNAUTICALMILE = 1.852;
+        final double MILESINNAUTICALMILE = 1.150779;
+        String stringNauticalMiles = JOptionPane.showInputDialog(null,
+                "Enter the number of quarts needed for the job:",
+                "Purchases",
+                JOptionPane.INFORMATION_MESSAGE);
+        double nauticalMiles= Double.parseDouble(stringNauticalMiles);
+        double kilometers = KMINNAUTICALMILE * nauticalMiles;
+        double miles = MILESINNAUTICALMILE * nauticalMiles;
+
+        JOptionPane.showMessageDialog(null,"There are " + kilometers + " kilometers in " +
+                nauticalMiles + " nautical miles.\nThere are " + miles + " miles in " + nauticalMiles +
+                " nautical miles.");
     }
 }
