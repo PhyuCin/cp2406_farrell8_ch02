b053f03021a65b43e8526bbcd97bbca93c8d14c6
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-22 12:10:10 +0800

    complete InchesToFeetInteractive

---
+++ Prac 02/InchesToFeetInteractive.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class InchesToFeetInteractive {
+    public static void main(String[] arg){
+        final int INCHESINFOOT = 12;
+        String stringInchesToConvert = JOptionPane.showInputDialog(null,
+                "Enter the number of inches to convert:",
+                "Inches to convert",
+                JOptionPane.INFORMATION_MESSAGE);
+        int inchesToConvert = Integer.parseInt(stringInchesToConvert);
+        int feet = inchesToConvert / INCHESINFOOT;
+        int inches = inchesToConvert % INCHESINFOOT;
+
+        System.out.println("There are " + feet + " feets and " + inches + " inches in " + inchesToConvert + " inches.");
+
+    }
+}
